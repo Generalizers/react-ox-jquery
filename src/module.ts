c0e4486572bfef19d9aecdb108e1fc5124f6cf5d
Alan Blanchet <alan@alan-blanchet.fr>
2022-01-30 17:27:21 +0100

    [FIX]Library export relative path

--- src/module.ts
+++ src/module.ts
@@ -2,6 +2,6 @@ import $ from 'jquery';
 import 'jquery-ui-bundle';
 import 'jquery-ui-bundle/jquery-ui.min.css';
 
-import { DragDrop } from 'components/DragDrop';
+import { DragDrop } from './components/DragDrop';
 
 export { DragDrop };
